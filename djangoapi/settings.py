1cf49d969027f56a4b036a1ac16e469e5709b2d9
Gavriil1 G <airbnbguide@gmail.com>
2023-12-24 14:09:11 +0000

    added if 'CLIENT_ORIGIN_DEV' in os.environ: to fix authentication

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -107,7 +107,7 @@ if 'CLIENT_ORIGIN' in os.environ:
      CORS_ALLOWED_ORIGINS = [
          os.environ.get('CLIENT_ORIGIN')
      ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
      CORS_ALLOWED_ORIGIN_REGEXES = [
          r"^https://.*\.gitpod\.io$",
      ]
