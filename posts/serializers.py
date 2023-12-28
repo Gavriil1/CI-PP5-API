e3a840da6bc8ed22539cabc1f93cf4ae94f80b2b
Gavriil1 G <airbnbguide@gmail.com>
2023-12-28 14:35:12 +0000

    added urllib3==1.26.18

--- posts/serializers.py
+++ posts/serializers.py
@@ -26,26 +26,6 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
-    # def validate_image(self, value):
-    #     if value.size > 2 * 1024 * 1024:
-    #         raise serializers.ValidationError('Image size larger than 2MB!')
-        
-    #     # Check image dimensions only if it's an image file
-    #     if hasattr(value, 'image') and hasattr(value.image, 'height') and hasattr(value.image, 'width'):
-    #         if value.image.height > 4096:
-    #             raise serializers.ValidationError('Image height larger than 4096px!')
-    #         if value.image.width > 4096:
-    #             raise serializers.ValidationError('Image width larger than 4096px!')
-
-    #         return value
-    # def validate_image(self, value):
-    #     if value.size > 2 * 1024 * 1024:
-    #         raise serializers.ValidationError('Image size larger than 2MB!')
-    #     if value.height > 4096:
-    #         raise serializers.ValidationError('Image height larger than 4096px!')
-    #     if value.width > 4096:
-    #         raise serializers.ValidationError('Image width larger than 4096px!')
-    #     return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
