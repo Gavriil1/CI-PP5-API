e0ae8bb0d4472e351ec511aebcb8e70bb6c0fd3a
Gavriil1 G <airbnbguide@gmail.com>
2023-12-28 13:45:23 +0000

    fixed validate_image variable. because react failed to con0nect

--- posts/serializers.py
+++ posts/serializers.py
@@ -12,18 +12,31 @@ class PostSerializer(serializers.ModelSerializer):
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
 
+    # def validate_image(self, value):
+    #     if value.size > 2 * 1024 * 1024:
+    #         raise serializers.ValidationError('Image size larger than 2MB!')
+    #     if value.image.height > 4096:
+    #         raise serializers.ValidationError(
+    #             'Image height larger than 4096px!'
+    #         )
+    #     if value.image.width > 4096:
+    #         raise serializers.ValidationError(
+    #             'Image width larger than 4096px!'
+    #         )
+    #     return value
+
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
             raise serializers.ValidationError('Image size larger than 2MB!')
-        if value.image.height > 4096:
-            raise serializers.ValidationError(
-                'Image height larger than 4096px!'
-            )
-        if value.image.width > 4096:
-            raise serializers.ValidationError(
-                'Image width larger than 4096px!'
-            )
-        return value
+        
+        # Check image dimensions only if it's an image file
+        if hasattr(value, 'image') and hasattr(value.image, 'height') and hasattr(value.image, 'width'):
+            if value.image.height > 4096:
+                raise serializers.ValidationError('Image height larger than 4096px!')
+            if value.image.width > 4096:
+                raise serializers.ValidationError('Image width larger than 4096px!')
+
+            return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
